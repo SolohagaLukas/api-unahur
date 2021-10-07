f6530ce5f89544cb97092996d7cfebcea74b4d40
Solohaga Lukas <solohagalukas@gmail.com>
2021-10-06 22:34:29 -0300

    Se agrega la entidad Alumno

--- api/models/alumno.js
+++ api/models/alumno.js
@@ -1,16 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const materia = sequelize.define('materia', {
+  const alumno = sequelize.define('alumno', {
     nombre: DataTypes.STRING,
     id_carrera: DataTypes.INTEGER
   }, {});
-  materia.associate = function(models) {
+  alumno.associate = function(models) {
     // associations can be defined here
-    materia.belongsTo(models.carrera
+    alumno.belongsTo(models.carrera
       ,{
         as: 'Carrera-Relacionada',
         foreignKey: 'id_carrera'
       })
   };
-  return materia;
+  return alumno;
 };
